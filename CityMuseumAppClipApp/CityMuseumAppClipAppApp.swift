ac41473b64087cb5720312f15d1fc47a847d0ace
Manas Vijaywargiya <manasdevslopes@gmail.com>
2025-08-01 12:35:15 +0530

    Added steps to test AppClips locally in iPhone

--- CityMuseumAppClipApp/CityMuseumAppClipAppApp.swift
+++ CityMuseumAppClipApp/CityMuseumAppClipAppApp.swift
@@ -27,5 +27,16 @@ struct CityMuseumAppClipAppApp: App {
 // - https://developer.apple.com/documentation/appclip/creating-app-clip-codes-with-the-app-clip-code-generator
 // - Apple Developer Downloads site
 // - https://developer.apple.com/download/all/?q=App%20Clip
+// - One more example of Coffee App AppClip
+// - http://bugfender.com/blog/creating-ios-app-clips/
 
+// MARK: - Steps to test AppClips Locally on iPhone
+/*
+ - Build and run our AppClip on a iPhone. We’ll need to run the AppClip first.
+ - On the iPhone, open Settings and navigate to the Developer > Local Experiences, then select ‘Register Local Experience’
+ - Enter the invocation URL we want to test – in our case: https://citymuseum.com/exhibit/1
+ - Enter the bundle ID of the AppClip
+ - Provide a title and a subtitle along with image
+ - Select an Action; for example Open
+*/
 
